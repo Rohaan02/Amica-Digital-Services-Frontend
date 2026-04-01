69d676af8f0e3470edc1621ad63a4a469ae57d97
Rohaan02 <rohaannadeem2@gmail.com>
2026-04-01 09:24:56 +0500

    resolved import of useState was missing in Pricing.

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import PricingCard from "../components/PricingCard";
