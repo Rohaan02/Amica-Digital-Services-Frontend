8689928c52aa419e4810f9c8e1f6dea9a0f7389f
Rohaan02 <rohaannadeem2@gmail.com>
2026-04-06 15:12:18 +0500

    fixed(caseStudy): useState is not defined

--- src/pages/CaseStudy.jsx
+++ src/pages/CaseStudy.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
